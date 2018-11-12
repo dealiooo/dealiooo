b6940fad212e257dd4f5ace5fa6778dc1759de46
mwinata94 <mwinata@mail.sfsu.edu>
2018-11-11 18:27:04 -0800

    fixed default path

--- server/routes/DefaultRoute.js
+++ server/routes/DefaultRoute.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var path = require('path');
 
 router.all('*', (request, response) => {
-  response.sendFile(path.join(__dirname + '../../client/build/index.html'));
+  response.sendFile(path.join(__dirname + '../../../client/public/index.html'));
 });
 
 module.exports = router;
