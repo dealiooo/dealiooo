4aa6fdd8eafc2ec8b7d2bbdee242d12475728321
mwinata94 <mwinata@mail.sfsu.edu>
2018-11-13 19:29:33 -0800

    integrate bulma on game lobby

--- client/src/containers/Lobby/Game/GameLobby.js
+++ client/src/containers/Lobby/Game/GameLobby.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
-import './GameLobby.css';
+import Section from 'react-bulma-components/lib/components/section';
+import Level from 'react-bulma-components/lib/components/level';
+import Box from 'react-bulma-components/lib/components/box';
+import Button from 'react-bulma-components/lib/components/button';
 
 class MainLobby extends Component {
   render() {
@@ -11,17 +14,38 @@ class MainLobby extends Component {
       gameLobbyName = 'Not Registered';
     }
     return (
-      <div>
-        Game Lobby - {gameLobbyName}
-        <div className="chatPlaceholder" />
-        <div className="buttons">
-          <button className="startButton">Start</button>
-          <button className="exitButton">Exit</button>
-          <button className="readyButton">Ready</button>
-        </div>
-      </div>
+      <Box>
+        <Section className="has-text-centered">navBar goes here</Section>
+        <Section className="has-text-centered">
+          Game Lobby - {gameLobbyName}
+        </Section>
+        <Section className="has-text-centered">chat goes here</Section>
+        <Section>
+          <Level>
+            <Level.Item />
+            <Level.Item />
+            <Level.Item>
+              <StartButton host={this.props.host} />
+            </Level.Item>
+            <Level.Item>
+              <Button className="is-large">Ready</Button>
+            </Level.Item>
+            <Level.Item>
+              <Button className="is-large">Exit</Button>
+            </Level.Item>
+            <Level.Item />
+          </Level>
+        </Section>
+      </Box>
     );
   }
+}
+
+function StartButton(props) {
+  if (props.host) {
+    return <Button className="is-large">Start</Button>;
+  }
+  return <div />;
 }
 
 export default MainLobby;
