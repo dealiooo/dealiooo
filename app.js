a1a1c90ed2b7b7050b345932a3fbb42cc02eace9
Lewbeisi <Stanleyliu15@gmail.com>
2018-11-16 10:55:21 -0800

    clean up

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
+const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
 
-const path = require('path');
-const express = require('express');
 const routes = require('./server/routes');
+const session = require('./server/database/config/session');
 const sockets = require('./server/sockets');
-
 const { passport } = require('./server/authentication/passport');
 
 const app = express();
-const bodyParser = require('body-parser');
-app.use(require('morgan')('dev'));
+app.sockets = sockets;
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session);
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(require('cookie-parser')());
-app.use(require('./server/database/config/session'));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.sockets = sockets;
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(routes);
 
 module.exports = app;
