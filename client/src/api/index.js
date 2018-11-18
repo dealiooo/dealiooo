1d2a160121ddbf91b9fa6401bae96396e3c95d9c
mwinata94 <mwinata@mail.sfsu.edu>
2018-11-18 12:39:28 -0800

    implement fetch with cors

---
+++ client/src/api/index.js
@@ -0,0 +1,59 @@
+const connection_type = 'http://';
+const website = 'localhost';
+const port_num = ':8000';
+
+const check_status = response => {
+  console.log('check_status');
+  console.log(response);
+  if (response.ok) {
+    return response;
+  }
+  return Promise.reject(new Error(`Response status was ${response.status}`));
+};
+
+const request_configuration = (body = {}, method) => {
+  const config = {
+    method,
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  if (Object.keys(body).length > 0) {
+    config.body = JSON.stringify(body);
+  }
+
+  return config;
+};
+
+const build_url = endpoint => {
+  return `${connection_type}${website}${port_num}${endpoint}`;
+};
+
+const request = (endpoint, body, method = 'post') =>
+  fetch(build_url(endpoint), request_configuration(body, method)).then(
+    check_status
+  );
+
+const jsonify = response => response.json();
+
+export default {
+  register: (name, email, password) =>
+    request('/register', { name: name, email: email, password: password }).then(
+      jsonify
+    ),
+  login: (email, password) =>
+    request('/login', { email: email, password: password }).then(jsonify),
+  send_chat: (room_id, message) => request(`/chat/${room_id}`, { message }),
+  //get_game_lobby_list: () => request('/games', {}, 'get').then(jsonify),
+  //get_game_info: game_id => request(`game-info/${game_id}`, {}, 'get').then(jsonify),
+  //get_create_game: () => request('', {}, 'get'),
+  //get_join_game: () => request('games/create', {}),
+  //get_leave_game: () => request('games/create', {}, 'get'),
+  //get_run_game: () => request('games/create', {}, 'get'),
+  post_create_game: game_id => request(`/create-game/${game_id}`, {}),
+  post_join_game: game_id => request(`/join-game/${game_id}`, {}),
+  post_leave_game: game_id => request(`/leave-game/${game_id}`, {}),
+  post_run_game: game_id => request(`/run-game/${game_id}`, {})
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,23 @@ const AuthRoute = require('./AuthRoute');
 const SocketRoute = require('./SocketRoute');
 const DefaultRoute = require('./DefaultRoute');
 
+const connection_type = 'http://';
+const website = 'localhost';
+const port_num = ':3000';
+
+router.use(function(request, response, next) {
+  response.header(
+    'Access-Control-Allow-Origin',
+    `${connection_type}${website}${port_num}`
+  );
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  response.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.use(AuthRoute);
 router.use(SocketRoute);
 // All routes not specified above will default to this route
