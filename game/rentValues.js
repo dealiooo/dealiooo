ccb734c552ec223e9121dedf3a2c00ffa5f88b84
Lewbeisi <Stanleyliu15@gmail.com>
2018-11-28 14:54:28 -0800

    Upload Hard-Coded Cards and Rent Values

---
+++ game/rentValues.js
@@ -0,0 +1,64 @@
+/*
+* RentValues Format
+* {
+*    color: String,
+*    rentValues: [Integer],
+*    maxSize: Integer
+* }
+*
+*/
+
+const rentValues = [
+  {
+    color: 'green',
+    rentValues: [2, 4, 7],
+    maxSize: 3
+  },
+  {
+    color: 'dark-blue',
+    rentValues: [3, 8],
+    maxSize: 2
+  },
+  {
+    color: 'light-blue',
+    rentValues: [1, 2, 3],
+    maxSize: 3
+  },
+  {
+    color: 'red',
+    rentValues: [2, 3, 6],
+    maxSize: 3
+  },
+  {
+    color: 'utility',
+    rentValues: [1, 2],
+    maxSize: 2
+  },
+  {
+    color: 'yellow',
+    rentValues: [2, 4, 6],
+    maxSize: 3
+  },
+  {
+    color: 'orange',
+    rentValues: [1, 3, 5],
+    maxSize: 3
+  },
+  {
+    color: 'brown',
+    rentValues: [1, 2],
+    maxSize: 2
+  },
+  {
+    color: 'purple',
+    rentValues: [1, 2, 4],
+    maxSize: 3
+  },
+  {
+    color: 'railroad',
+    rentValues: [1, 2, 3, 4],
+    maxSize: 4
+  }
+];
+
+export default rentValues;
