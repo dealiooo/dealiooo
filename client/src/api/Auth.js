c89b0b3415d8fcfb6cc46cf0ff18918e4dd2ede4
Stanley <Stanleyliu15@gmail.com>
2019-03-16 17:57:32 -0700

    update api for new password

--- client/src/api/Auth.js
+++ client/src/api/Auth.js
@@ -1,15 +1,18 @@
-const request = require('./request');
+const request = require("./request");
 
 const jsonify = response => response.json();
 
 module.exports = {
-  getLogin: () => request('/login', {}, 'get'),
-  getRegister: () => request('/register', {}, 'get'),
-  getForgotPassword: () => request('/forgot-password', {}, 'get'),
+  getLogin: () => request("/login", {}, "get"),
+  getRegister: () => request("/register", {}, "get"),
+  getForgotPassword: () => request("/forgot-password", {}, "get"),
+  getNewPassword: sid => request(`/new-password/${sid}`, {}, "get"),
   postLogin: (email, password) =>
-    request('/login', { email, password }).then(jsonify),
+    request("/login", { email, password }).then(jsonify),
   postRegister: (name, email, password) =>
-    request('/register', { name, email, password }),
-  postForgotPassword: email => request('/forgot-password', { email }),
-  postLogout: () => request('/logout', {})
-}
+    request("/register", { name, email, password }),
+  postForgotPassword: email => request("/forgot-password", { email }),
+  postNewPassword: (sid, password) =>
+    request(`/new-password/${sid}`, { password }),
+  postLogout: () => request("/logout", {})
+};
