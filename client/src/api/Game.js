dfec42413a967ebe64ccb92d18dde553261a873d
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-04 16:45:59 -0700

    add loadGame api to frontend

--- client/src/api/Game.js
+++ client/src/api/Game.js
@@ -1,14 +1,15 @@
 const request = require('./request');
 
 module.exports = {
-    getGame: gameId => request(`/game/${gameId}`, {}, 'get'),
-    postGameChat: ({ roomId: gameId, message }) =>
+  getGame: gameId => request(`/game/${gameId}`, {}, 'get'),
+  postGameChat: ({ roomId: gameId, message }) =>
     request(`/game/${gameId}/chat`, { message }),
-    postGameClick: (gameId, clickInput) =>
+  postGameClick: (gameId, clickInput) =>
     request(`/game/${gameId}/click`, { clickInput }),
-    postGameEndTurn: gameId => request(`/game/${gameId}/endTurn`, {}),
-    postGameForfeit: gameId => request(`/game/${gameId}/forfeit`, {}),
-    postGameJoin: gameId => request(`/game/${gameId}/join`, {}),
-    postGameStartGame: gameId => request(`/game/${gameId}/startGame`, {}),
-    postGameUpdate: gameId => request(`/game/${gameId}/update`, {})
-}
+  postGameEndTurn: gameId => request(`/game/${gameId}/endTurn`, {}),
+  postGameForfeit: gameId => request(`/game/${gameId}/forfeit`, {}),
+  postGameJoin: gameId => request(`/game/${gameId}/join`, {}),
+  postGameStartGame: gameId => request(`/game/${gameId}/startGame`, {}),
+  postGameLoadGame: gameId => request(`/game/${gameId}/loadGame`, {}),
+  postGameUpdate: gameId => request(`/game/${gameId}/update`, {})
+};
