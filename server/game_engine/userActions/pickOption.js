39bd04fd756c925e6aa59e161ea24947fde49672
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-04 16:59:23 -0700

    omit options in promptMessage

--- server/game_engine/userActions/pickOption.js
+++ server/game_engine/userActions/pickOption.js
@@ -1,9 +1,9 @@
-const setPending = ({Game, requiredPlayer, options, message, callback}) => {
+const setPending = ({ Game, requiredPlayer, options, message, callback }) => {
   if (!message) {
     message = [
       `Player id:${requiredPlayer.id}`,
-      `pick an option:`,
-      options.map((option, i) => `\n${i}: ${option}`).join('')
+      `pick an option:` /*,
+      options.map((option, i) => `\n${i}: ${option}`).join('')*/
     ].join('\n');
   }
   Game.pendingForUserInput = {
@@ -13,30 +13,37 @@ const setPending = ({Game, requiredPlayer, options, message, callback}) => {
   };
 };
 
-const pickOption = (
-  { Game, player: requiredPlayer, playerId, options, message, cancelled, forced, callback }
-) => {
+const pickOption = ({
+  Game,
+  player: requiredPlayer,
+  playerId,
+  options,
+  message,
+  cancelled,
+  forced,
+  callback
+}) => {
   if (playerId && requiredPlayer.id !== playerId) {
     return;
   }
   if (options.length) {
     if (cancelled) {
-      callback({cancelled});
+      callback({ cancelled });
     } else if (forced) {
-      callback({option:options[0], forced});
+      callback({ option: options[0], forced });
     } else if ('' === Game.userInput) {
-      setPending({Game, requiredPlayer, options, message, callback});
+      setPending({ Game, requiredPlayer, options, message, callback });
     } else {
       let option = parseInt(Game.userInput);
       if (-1 < option && option < options.length) {
         Game.userInput = '';
         Game.pendingForUserInput = null;
-        callback({option:options[option]});
+        callback({ option: options[option] });
         return `\nYou picked:  \n ◾ ${option}: ${options[option]}`;
       }
     }
   } else {
-    callback({error:'empty options'});
+    callback({ error: 'empty options' });
   }
 };
 
