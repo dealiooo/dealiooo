d1612e00d4dff1d975269fbe6ac2696820c463bb
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-11 17:02:05 -0700

    fix missing branch definition

--- server/game_engine/gameControls/playCard/playActionCard/playSlyDeal.js
+++ server/game_engine/gameControls/playCard/playActionCard/playSlyDeal.js
@@ -1,99 +1,140 @@
 const gameActions = require('../../../gameActions');
 const userControls = require('../../../userControls');
 
-const pickFieldCardToSlyDeal = ({Game, player, targetPlayer, forced, callback}) => {
+const pickFieldCardToSlyDeal = ({
+  Game,
+  player,
+  targetPlayer,
+  forced,
+  callback
+}) => {
   userControls.pickFieldCard({
     Game,
     player: targetPlayer,
     pileNames: ['propertyCards', 'buildingCards'],
     forced,
-    callback: ({error, card, source, cancelled, forced}) => {
+    callback: ({ error, card, source, cancelled, forced }) => {
       if (error) {
-        callback({error});
+        callback({ error });
       } else if (cancelled) {
-        console.log("slydeal: cant cancel this one")
-        pickFieldCardToSlyDeal({Game, player, targetPlayer, callback});
+        console.log('slydeal: cant cancel this one');
+        pickFieldCardToSlyDeal({ Game, player, targetPlayer, callback });
       } else {
-        processSlyDeal({Game, player, targetPlayer, card, source, forced, callback});
+        processSlyDeal({
+          Game,
+          player,
+          targetPlayer,
+          card,
+          source,
+          forced,
+          callback
+        });
       }
     }
   });
-}
+};
 
-const processSlyDeal = ({Game, player, targetPlayer, card, source, forced, callback}) => {
+const processSlyDeal = ({
+  Game,
+  player,
+  targetPlayer,
+  card,
+  source,
+  forced,
+  callback
+}) => {
   if ('propertyCards' === source.pileName) {
-    if (gameActions.getPropertySetStatus({Game, destination: source.pile})) {
+    if (gameActions.getPropertySetStatus({ Game, destination: source.pile })) {
       if (forced) {
-        forcedPickFieldCardToSlyDeal({Game, player, targetPlayer, callback});
+        forcedPickFieldCardToSlyDeal({ Game, player, targetPlayer, callback });
       } else {
-        pickFieldCardToSlyDeal({Game, player, targetPlayer, callback});
+        pickFieldCardToSlyDeal({ Game, player, targetPlayer, callback });
       }
     } else {
       player.field.propertyCards.push([]);
       gameActions.moveCard({
         source: source.pile,
-        destination: player.field.propertyCards[player.field.propertyCards.length - 1],
+        destination:
+          player.field.propertyCards[player.field.propertyCards.length - 1],
         card
       });
-      gameActions.removeEmptyPropertySets({player: targetPlayer});
-      callback({card});
+      gameActions.removeEmptyPropertySets({ player: targetPlayer });
+      callback({ card });
     }
   } else {
-    gameActions.moveCard({source: source.pile, destination: player.field[source.pileName], card});
-    callback({card});
+    gameActions.moveCard({
+      source: source.pile,
+      destination: player.field[source.pileName],
+      card
+    });
+    callback({ card });
   }
 };
 
-const forcedPickFieldCardToSlyDeal = ({Game, player, targetPlayer, callback}) => {
+const forcedPickFieldCardToSlyDeal = ({
+  Game,
+  player,
+  targetPlayer,
+  callback
+}) => {
   let canSlyDeal = false;
   let card, source;
   if (0 !== targetPlayer.field['buildingCards'].length) {
-    card = targetPlayer.field['buildingCards'][0],
-    source = {
-      pileName: 'buildingCards',
-      pile: targetPlayer.field['buildingCards']
-    }
+    (card = targetPlayer.field['buildingCards'][0]),
+      (source = {
+        pileName: 'buildingCards',
+        pile: targetPlayer.field['buildingCards']
+      });
     canSlyDeal = true;
   } else {
     let size = targetPlayer.field['propertyCards'].length;
     for (let i = 0; i < size; i++) {
-      if (gameActions.getPropertySetStatus({Game, destination: targetPlayer.field['propertyCards'][i]})) {
+      if (
+        gameActions.getPropertySetStatus({
+          Game,
+          destination: targetPlayer.field['propertyCards'][i]
+        })
+      ) {
         card = targetPlayer.field['propertyCards'][i][0];
         source = {
           pileName: 'propertyCards',
           pile: targetPlayer.field['propertyCards'][i]
-        }
+        };
         canSlyDeal = true;
         break;
       }
     }
   }
   if (canSlyDeal) {
-    processSlyDeal({Game, player, targetPlayer, card, source, callback});
+    processSlyDeal({ Game, player, targetPlayer, card, source, callback });
   } else {
-    callback({card});
+    callback({ card });
   }
-}
+};
 
-module.exports = ({Game, player, card, callback}) => {
+module.exports = ({ Game, player, card, callback }) => {
   userControls.pickTargetPlayer({
-    Game, 
-    player, 
-    callback: ({error, targetPlayer, cancelled, forced}) => {
+    Game,
+    player,
+    callback: ({ error, targetPlayer, cancelled, forced }) => {
       if (error || cancelled || forced) {
-        callback({error, cancelled, forced});
+        callback({ error, cancelled, forced });
       } else {
-        gameActions.moveCard({source: player.hand, destination: player.field.actionCards, card});
-        gameActions.onNonCounterCardPlayed({Game, card});
+        gameActions.moveCard({
+          source: player.hand,
+          destination: player.field.actionCards,
+          card
+        });
+        gameActions.onNonCounterCardPlayed({ Game, card });
         gameActions.avoidAction({
-          Game, 
-          player: targetPlayer, 
-          sourcePlayer: player, 
-          callback: ({avoid, forced}) => {
+          Game,
+          player: targetPlayer,
+          sourcePlayer: player,
+          callback: ({ avoid, forced }) => {
             if (avoid || forced) {
-              callback({card});
+              callback({ card });
             } else {
-              
+              pickFieldCardToSlyDeal({ Game, player, targetPlayer, callback });
             }
           }
         });
