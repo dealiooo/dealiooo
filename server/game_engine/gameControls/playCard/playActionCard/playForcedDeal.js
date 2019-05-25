f37ad9179b4f324f75e0ec2f12039d177d198cb8
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-25 12:24:25 -0700

    fix missing Game reference

--- server/game_engine/gameControls/playCard/playActionCard/playForcedDeal.js
+++ server/game_engine/gameControls/playCard/playActionCard/playForcedDeal.js
@@ -1,45 +1,58 @@
 const gameActions = require('../../../gameActions');
 const userControls = require('../../../userControls');
 
-const playForcedDeal = ({Game, player, card, callback}) => {
+const playForcedDeal = ({ Game, player, card, callback }) => {
   userControls.pickFieldCard({
     Game,
     player,
     pileNames: ['propertyCards', 'buildingCards'],
-    callback: ({error, card: playerCard, source: playerSource, cancelled, forced}) => {
+    callback: ({
+      error,
+      card: playerCard,
+      source: playerSource,
+      cancelled,
+      forced
+    }) => {
       if (error || cancelled || forced) {
-        callback({error, cancelled, forced});
+        callback({ error, cancelled, forced });
       } else {
         userControls.pickTargetPlayer({
-          player, 
-          callback: ({error, targetPlayer, cancelled, forced}) => {
-          if (error || forced) {
-            callback({error, forced});
-          } else if (cancelled) {
-            playForcedDeal({Game, player, card, callback});
-          } else {
-            gameActions.moveCard({source: player.hand, destination: player.field.actionCards, card});
-            gameActions.onNonCounterCardPlayed({Game, card});
-            gameActions.avoidAction({
-              Game, 
-              player: targetPlayer, 
-              sourcePlayer: player, 
-              callback: ({avoid, forced}) => {
-              if (avoid || forced) {
-                callback({card, forced});
-              } else {
-                pickCardToForcedDeal({
-                  Game,
-                  player,
-                  playerCard,
-                  playerSource,
-                  targetPlayer,
-                  callback
-                });
-              }
-            }});
+          Game,
+          player,
+          callback: ({ error, targetPlayer, cancelled, forced }) => {
+            if (error || forced) {
+              callback({ error, forced });
+            } else if (cancelled) {
+              playForcedDeal({ Game, player, card, callback });
+            } else {
+              gameActions.moveCard({
+                source: player.hand,
+                destination: player.field.actionCards,
+                card
+              });
+              gameActions.onNonCounterCardPlayed({ Game, card });
+              gameActions.avoidAction({
+                Game,
+                player: targetPlayer,
+                sourcePlayer: player,
+                callback: ({ avoid, forced }) => {
+                  if (avoid || forced) {
+                    callback({ card, forced });
+                  } else {
+                    pickCardToForcedDeal({
+                      Game,
+                      player,
+                      playerCard,
+                      playerSource,
+                      targetPlayer,
+                      callback
+                    });
+                  }
+                }
+              });
+            }
           }
-        }});
+        });
       }
     }
   });
@@ -57,11 +70,24 @@ const pickCardToForcedDeal = ({
     Game,
     player: targetPlayer,
     pileNames: ['propertyCards', 'buildingCards'],
-    callback: ({error, card: targetPlayerCard, source: targetPlayerSource, cancelled, forced}) => {
+    callback: ({
+      error,
+      card: targetPlayerCard,
+      source: targetPlayerSource,
+      cancelled,
+      forced
+    }) => {
       if (error) {
-        callback({error});
+        callback({ error });
       } else if (cancelled) {
-        pickCardToForcedDeal(Game, player, playerCard, playerSource, targetPlayer, callback);
+        pickCardToForcedDeal(
+          Game,
+          player,
+          playerCard,
+          playerSource,
+          targetPlayer,
+          callback
+        );
       } else {
         forcedDeal({
           Game,
@@ -72,7 +98,7 @@ const pickCardToForcedDeal = ({
           targetPlayerCard,
           targetPlayerSource
         });
-        callback({card});
+        callback({ card });
       }
     }
   });
@@ -88,8 +114,14 @@ const forcedDeal = ({
   targetPlayerSource
 }) => {
   if (
-    !gameActions.getPropertySetStatus({Game, propertySet: playerSource.pile}) &&
-    !gameActions.getPropertySetStatus({Game, propertySet: targetPlayerSource.pile})
+    !gameActions.getPropertySetStatus({
+      Game,
+      propertySet: playerSource.pile
+    }) &&
+    !gameActions.getPropertySetStatus({
+      Game,
+      propertySet: targetPlayerSource.pile
+    })
   ) {
     gameActions.swapPropertyCards({
       source: { card: playerCard, pile: playerSource.pile, player },
