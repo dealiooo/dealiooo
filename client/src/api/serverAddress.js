0d6f0b61b04711b6cb4700daeb94e4a2927c5a50
Stanley <Stanleyliu15@gmail.com>
2019-05-25 20:01:05 -0700

    Fix build: remove extra / from herokuapp

--- client/src/api/serverAddress.js
+++ client/src/api/serverAddress.js
@@ -2,7 +2,7 @@ const connectionType = 'http://';
 const website = 'localhost';
 const portNum = ':8000';
 
-const herokuAddress = 'https://dealiooo.herokuapp.com/';
+const herokuAddress = 'https://dealiooo.herokuapp.com';
 
 module.exports = process.env.REACT_APP_LOCAL_SERVER
   ? connectionType + website + portNum
