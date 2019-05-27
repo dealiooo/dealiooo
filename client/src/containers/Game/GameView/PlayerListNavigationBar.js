99e5c0efc8000e4099fd315882ea7817a4a8f03b
Stanley <Stanleyliu15@gmail.com>
2019-05-27 01:07:03 -0700

    make the player list items expand vertically with flexbox and add hover style

--- client/src/containers/Game/GameView/PlayerListNavigationBar.js
+++ client/src/containers/Game/GameView/PlayerListNavigationBar.js
@@ -8,30 +8,68 @@ class PlayerListNavigationBar extends Component {
       playerInfos,
       selectedPlayerId,
       onPlayerClick,
-      onPlayerHover
+      onPlayerHover,
+      contentHeight
     } = this.props;
 
     return (
-      <aside className="menu">
-        <ul className="menu-list">
-          {playerInfos.map(playerInfo => (
+      <aside
+        className="menu"
+        style={{
+          // 4: marginBottoms
+          height: `${contentHeight - playerInfos.length * 4 - 4}px`
+        }}
+      >
+        <ul
+          className="menu-list"
+          style={{
+            width: `100%`,
+            height: `100%`,
+            display: `flex`,
+            flexDirection: `column`,
+            alignItems: `stretch`
+          }}
+        >
+          {playerInfos.map((playerInfo, i) => (
             <li
               className={`${
                 selectedPlayerId === playerInfo.id ? 'is-active' : ''
               }`}
+              key={playerInfo.id}
+              style={{
+                display: `flex`,
+                flexGrow: `1`,
+                flexDirection: `row`,
+                justifyContent: `center`,
+                alignItems: `center`,
+                alignSelf: `stretch`,
+                width: `100%`,
+                height: `100%`,
+                marginBottom: i === playerInfos.length - 1 ? `0px` : `4px`
+              }}
             >
               <Hoverable
                 data={{ hoverPlayerId: playerInfo.id }}
                 onEnter={onPlayerHover}
+                style={{
+                  width: `100%`,
+                  height: `100%`
+                }}
               >
                 <button
-                  className={`button is-fullwidth has-text-centered ${
-                    playerInfo.id === selectedPlayerId ? 'is-primary' : ''
+                  className={`button is-fullwidth has-text-centered global-dark-hover ${
+                    selectedPlayerId === playerInfo.id
+                      ? 'has-background-dark'
+                      : ''
                   }`}
+                  style={{
+                    height: `100%`,
+                    color: `${selectedPlayerId === playerInfo.id ? 'gold' : ''}`
+                  }}
                   value={playerInfo.id}
                   onClick={onPlayerClick}
                 >
-                  {playerInfo.id}
+                  {playerInfo.username}
                 </button>
               </Hoverable>
             </li>
