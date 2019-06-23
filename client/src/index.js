8e4dcbc881a5f0097e7386f6e5eb8625578bafdf
Stanley <Stanleyliu15@gmail.com>
2019-06-22 18:55:39 -0700

    reference bulma in react

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import './style.css';
+import 'bulma/css/bulma.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
