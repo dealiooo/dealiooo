837de78460d225780533b297242023c211cdd91f
Stanley <Stanleyliu15@gmail.com>
2019-11-06 17:08:40 -0800

    fix: remove unnecessary wrapper on loader

--- client/src/components/molecules/Loader/index.tsx
+++ client/src/components/molecules/Loader/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
 import { Loader as LoaderIcon } from 'styled-icons/feather/Loader';
+import styled, { keyframes } from 'styled-components';
 
 import { IconProps } from '../../atoms';
-import { IconWrapper } from './style';
 
 export type Props = Omit<IconProps, 'icon'> & {};
 
-export default function(props: Props) {
-  return (
-    <IconWrapper>
-      <LoaderIcon {...props} />
-    </IconWrapper>
-  );
-}
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export default styled(LoaderIcon)`
+  animation: ${rotate} infinite 1s linear;
+`;
