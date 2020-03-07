62a045e4951ed43e2e607138626e456a06f9688d
Stanley <Stanleyliu15@gmail.com>
2020-03-07 14:31:02 -0800

    feat: add change theme in moremenu

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -4,17 +4,27 @@ import { constants, mixins } from '../../styles';
 
 const { spaces } = constants;
 
+const MenuItem = styled.li`
+  ${mixins.flexRowCenter};
+  width: 100%;
+
+  & > * {
+    padding: ${spaces.md} ${spaces.xl};
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.background};
+  }
+`;
+
 const Menu = styled.ul`
   ${mixins.roundBox};
   ${mixins.flexColCenter};
   background-color: ${({ theme }) => theme.palette.backgroundLighter};
   padding: ${spaces.md};
-`;
 
-const MenuItem = styled.li`
-  padding: ${spaces.md} ${spaces.xl};
-  &:hover {
-    background-color: ${({ theme }) => theme.palette.background};
+  & > ${MenuItem}:nth-child(even) {
+    margin-top: ${spaces.sm};
   }
 `;
 
